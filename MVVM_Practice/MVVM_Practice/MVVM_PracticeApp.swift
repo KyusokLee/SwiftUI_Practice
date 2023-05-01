a6a3617f21e3f99b99caaad3c837ea7ce2fccaf0
KyusokLee <kyusok1@naver.com>
2023-05-01 22:43:08 +0900

    Create_ MVVM Folder Create

---
+++ MVVM_Practice/MVVM_Practice/MVVM_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_PracticeApp.swift
+//  MVVM_Practice
+//
+//  Created by Kyus'lee on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
