a55035b5f94292c4232b08d2bff2826c1d3072b1
KyusokLee <kyusok1@naver.com>
2023-05-01 23:08:50 +0900

    Create_ Model and View Practice

---
+++ MVVM_Practice/MVVM_Practice/Model/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  MVVM_Practice
+//
+//  Created by Kyus'lee on 2023/05/01.
+//
+
+import Foundation
---
+++ MVVM_Practice/MVVM_Practice/View/UserModelBasicView.swift
@@ -0,0 +1,20 @@
+//
+//  UserModelBasicView.swift
+//  MVVM_Practice
+//
+//  Created by Kyus'lee on 2023/05/01.
+//
+
+import SwiftUI
+
+struct UserModelBasicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserModelBasicView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserModelBasicView()
+    }
+}
